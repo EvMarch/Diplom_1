4a090e5533548be0ff7d97e7437baad891168684
EvMarch <evmarch92@gmail.com>
2024-10-25 04:01:53 +0300

    2

---
+++ helpers.py
@@ -0,0 +1,20 @@
+class Helpers:
+    BUN = {
+        'name': 'grain bun',
+        'price': 250
+    }
+
+    SAUCE = {
+        'ingredient_type': 'SAUCE',
+        'name': 'tartar sauce',
+        'price': 250
+    }
+    FILLING = {
+        'ingredient_type': 'FILLING',
+        'name': 'fish',
+        'price': 250
+    }
+
+
+
+
--- tests/test_bun.py
+++ tests/test_bun.py
@@ -1,10 +1,13 @@
 from praktikum.bun import Bun
 
-
 class TestBun:
 
 
     def test_get_name(self):
-        bun = Bun('black bun', 100)
-        assert bun.get_name() =='black bun'
+        bun = Bun('grain bun', 250)
+        assert bun.get_name() =='grain bun'
+
+    def test_get_price(self):
+        bun = Bun('grain bun', 250)
+        assert bun.get_price() == 250
 
