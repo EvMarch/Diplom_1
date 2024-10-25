f6329aefb0b9a8566a00290e78a88f9781ceb853
EvMarch <evmarch92@gmail.com>
2024-10-25 09:05:27 +0300

    5

---
+++ tests/test_ingredient.py
@@ -0,0 +1,23 @@
+from ya_praktikum.ingredient import Ingredient
+from helpers import Helpers
+
+
+class TestIngredient:
+
+#тест проверяющий цену ингредиента
+
+    def test_get_price(self):
+        ingredient = Ingredient(Helpers.FILLING['type'], Helpers.FILLING['name'],Helpers.FILLING['price'])
+
+        assert ingredient.get_price() == 250
+
+#тест проверяющий название ингредиента
+    def test_get_name(self):
+        ingredient = Ingredient(Helpers.FILLING['type'], Helpers.FILLING['name'],Helpers.FILLING['price'])
+
+        assert ingredient.get_name() == 'fish'
+#тест проверяющий тип ингредиента
+    def test_get_type(self):
+        ingredient = Ingredient(Helpers.FILLING['type'], Helpers.FILLING['name'], Helpers.FILLING['price'])
+
+        assert ingredient.get_type() == 'FILLING'
